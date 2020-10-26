6b3d305fd1eadb4e1979a25a4c05234994ea1c6b
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2020-10-26 21:37:49 +0300

    Add files via upload

--- R/hw1_get_questions.R
+++ R/hw1_get_questions.R
@@ -8,9 +8,11 @@
 
 get_hw1_questions <- function(login = Sys.info()["user"]) {
   require(httr, stringr)
-  
+
   my_vle_login = login
-  
+
   r = httr::GET("https://pub.nosoc.io/content/101/hw_questions",
           query = list(vle_id = my_vle_login))
+
+  return(httr::content(r))
 }
