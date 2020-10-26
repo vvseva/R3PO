a4f38b4fe1dd5a99a33fcb2d4263949ebeb7caa1
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2020-10-27 01:22:22 +0300

    Update hw1_get_csv.R

--- R/hw1_get_csv.R
+++ R/hw1_get_csv.R
@@ -6,19 +6,16 @@
 #' @return csv file that will be saved loaded to your disk and environment
 #' @export
 
-
 get_hw1_airline_df <- function(login) {
-  require(httr)
-
+    require(httr)
+  
   if(missing(login)) {
-    my_vle_login = Sys.info()["user"]
+  my_vle_login = Sys.info()["user"]
   } else {my_vle_login = login}
 
-  my_vle_login = login
-
-  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_airline",
-                query = list(vle_id = my_vle_login),
-                httr::write_memory()
+  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_airline", 
+    query = list(vle_id = my_vle_login),
+    httr::write_memory()
   )
 
   content(r)
@@ -26,17 +23,15 @@ get_hw1_airline_df <- function(login) {
 }
 
 get_hw1_airport_df <- function(login) {
-  require(httr)
-
+    require(httr)
+  
   if(missing(login)) {
-    my_vle_login = Sys.info()["user"]
+  my_vle_login = Sys.info()["user"]
   } else {my_vle_login = login}
 
-  my_vle_login = login
-
-  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_airport",
-                query = list(vle_id = my_vle_login),
-                httr::write_memory()
+  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_airport", 
+    query = list(vle_id = my_vle_login),
+    httr::write_memory()
   )
 
   return(httr::content(r))
@@ -44,16 +39,14 @@ get_hw1_airport_df <- function(login) {
 
 get_hw1_seat_df <- function(login) {
   require(httr)
-
+  
   if(missing(login)) {
-    my_vle_login = Sys.info()["user"]
+  my_vle_login = Sys.info()["user"]
   } else {my_vle_login = login}
 
-  my_vle_login = login
-
-  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_seat",
-                query = list(vle_id = my_vle_login),
-                httr::write_memory()
+  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_seat", 
+    query = list(vle_id = my_vle_login),
+    httr::write_memory()
   )
 
   return(content(r))
@@ -61,16 +54,14 @@ get_hw1_seat_df <- function(login) {
 
 get_hw1_lounge_df <- function(login) {
   require(httr)
-
+  
   if(missing(login)) {
-    my_vle_login = Sys.info()["user"]
+  my_vle_login = Sys.info()["user"]
   } else {my_vle_login = login}
 
-  my_vle_login = login
-
-  r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_lounge",
-                query = list(vle_id = my_vle_login),
-                httr::write_memory()
+r = httr::GET("https://pub.nosoc.io/content/101/hw1_dataframe_lounge", 
+    query = list(vle_id = my_vle_login),
+    httr::write_memory()
   )
 
   return(httr::content(r))
