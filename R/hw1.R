e70d44887283bab37b9e3fe409030c48425b07be
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-08 22:20:22 +0300

    Add files via upload

--- R/hw1.R
+++ R/hw1.R
@@ -64,16 +64,15 @@ get_hw1_questions <- function(login) {
 #' This function loads a file in your environment
 #'
 #' @param login your vle login
-#' @param path place to write csv could be path, i.e. ~/my_folder/, or null
+#' @param df_name name of a dataset airline, airport, lounge or seat
 #' @return list of dataframes, also write csv files to your disk
 #' @export
 
-get_hw1_data <- function(login, path = NULL) {
+get_hw1_data <- function(login, df_name = "airline") {
   require(dplyr)
   require(stringr)
   require(kableExtra)
   require(readr)
-  # data(sysdata, envir = environment())
 
   if (missing(login)) {
     my_vle_login = Sys.info()["user"]
@@ -97,56 +96,57 @@ get_hw1_data <- function(login, path = NULL) {
 
     set.seed(seed)
 
-    airline <-  R3PO:::airline_sub_1 %>%
-      dplyr::slice_sample(prop = 1)
+    if (df_name == "airline") {
 
-    airport <-  R3PO:::airport_sub_1 %>%
-      dplyr::sample_n(1000) %>%
-      dplyr::bind_rows(
-        dplyr::tibble(
-          airport_name = "london-heathrow-airport",
-          link = "/watch?v=ZM04jn95YP0",
-          title = "London Heathrow Airport customer review",
-          author = my_vle_login,
-          author_country = "Russia",
-          date = Sys.Date(),
-          content = "Our first time through Heathrow airport, there was a guide to tell us if they wanted to rebook tickets to Heathrow, so if you are on an urgent flight and you ask, 'Are we going to change your book?' they don't reply. It's a very confusing process, they just tell you: 'Sorry, we did not meet the deadline, but we will do our best to help you.'
+      df_to_return <-  R3PO:::airline_sub_1 %>%
+        dplyr::slice_sample(prop = 1)
+
+    } else if (df_name == "airport") {
+
+      df_to_return <-  R3PO:::airport_sub_1 %>%
+        dplyr::sample_n(1000) %>%
+        dplyr::bind_rows(
+          dplyr::tibble(
+            airport_name = "london-heathrow-airport",
+            link = "/watch?v=ZM04jn95YP0",
+            title = "London Heathrow Airport customer review",
+            author = my_vle_login,
+            author_country = "Russia",
+            date = Sys.Date(),
+            content = "Our first time through Heathrow airport, there was a guide to tell us if they wanted to rebook tickets to Heathrow, so if you are on an urgent flight and you ask, 'Are we going to change your book?' they don't reply. It's a very confusing process, they just tell you: 'Sorry, we did not meet the deadline, but we will do our best to help you.'
           We can't do anything on the ground at Heathrow, so people have to pick up buses, buy a plane for the day and go in another part of the train. It's a really expensive process, as we have to do it in advance. As soon as we tell you the amount of time it takes, we have to tell them – sometimes we don't have someone on hand for more than 45 minutes. So we'll have to put our hand in hands.
           At Heathrow, an agent tells passengers, 'The first time we put on a tour agent, I was on a date with a London airport friend, and we went to Heathrow and tried to book a flight directly on to London Heathrow. There were a lot of people who told us that London Heathrow isn't what they expected it to be on the ground at Heathrow, and if we said 'No' to a group of people who asked us to give them their book, we couldn't understand how they were going to respond.",
-          experience_airport = "transit",
-          date_visit = "2011-11-11", type_traveller = 'Business',
-          overall_rating = 10, queuing_rating = 10,
-          terminal_cleanliness_rating = 10, terminal_seating_rating = 10,
-          terminal_signs_rating = 10, food_beverages_rating = 50,
-          airport_shopping_rating = 10, wifi_connectivity_rating = 10,
-          airport_staff_rating = 10, recommended = 1
-        )
-      ) %>%
-      dplyr::slice_sample(prop = 1)
+            experience_airport = "transit",
+            date_visit = "2011-11-11", type_traveller = 'Business',
+            overall_rating = 10, queuing_rating = 10,
+            terminal_cleanliness_rating = 10, terminal_seating_rating = 10,
+            terminal_signs_rating = 10, food_beverages_rating = 50,
+            airport_shopping_rating = 10, wifi_connectivity_rating = 10,
+            airport_staff_rating = 10, recommended = 1
+          )
+        ) %>%
+        dplyr::slice_sample(prop = 1)
 
-    lounge <- R3PO:::lounge_sub_1 %>%
-      dplyr::slice_sample(prop = 1)
+    } else if (df_name == "lounge") {
 
-    seat <- R3PO:::seat_sub_1 %>%
-      dplyr::slice_sample(prop = 1)
+      df_to_return <- R3PO:::lounge_sub_1 %>%
+        dplyr::slice_sample(prop = 1)
 
-    myList <- list(airline = airline,
-                   airport = airport,
-                   lounge = lounge,
-                   seat = seat)
+    } else if (df_name == "seat") {
 
-    mapply(write.csv, myList,
-           file=paste0(path, names(myList), '.csv'))
+      df_to_return <- R3PO:::seat_sub_1 %>%
+        dplyr::slice_sample(prop = 1)
 
-    if (is.null(path)) {
-      print(paste("Записал вам файлы в рабочую директорию", paste0(path, names(myList), '.csv')))
     } else {
-      print(paste("Записал вам файлы по этому пути", paste0(path, names(myList), '.csv')))
-    }
-    print("Удачи с дз")
-    print("Beep")
 
-    # return(myList)
+      print("wrong dataset name")
+      stop()
+
+    }
+
+    print(paste("держи", df_name))
+    return(df_to_return)
+    print("Beep")
 
     }
 
