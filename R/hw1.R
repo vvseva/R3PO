71a115b961386413eb9fd728f996dd25b31adac9
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-08 20:43:30 +0300

    Add files via upload

--- R/hw1.R
+++ R/hw1.R
@@ -23,25 +23,28 @@ get_hw1_questions <- function(login) {
   } else {
 
     seed = R3PO:::students_2021 %>%
-      filter(name == my_vle_login) %>%
-      select(random_seed) %>%
-      pull()
+      dplyr::filter(name == my_vle_login) %>%
+      dplyr::select(random_seed) %>%
+      dplyr::pull()
 
     set.seed(seed)
 
     df_q = R3PO:::hw1_questions
 
     q1 = df_q %>%
-      filter(Comment == "1 таблица") %>%
-      sample_n(1) %>% pull(var = Question)
+      dplyr::filter(Comment == "1 таблица") %>%
+      dplyr::sample_n(1) %>%
+      dplyr::pull(var = Question)
 
     q2 = df_q %>%
-      filter(Comment == "с текстами") %>%
-      sample_n(1) %>% pull(var = Question)
+      dplyr::filter(Comment == "с текстами") %>%
+      dplyr::sample_n(1) %>%
+      dplyr::pull(var = Question)
 
     q3 = df_q %>%
-      filter(Comment == "2 таблицы") %>%
-      sample_n(1) %>% pull(var = Question)
+      dplyr::filter(Comment == "2 таблицы") %>%
+      dplyr::sample_n(1) %>%
+      dplyr::pull(var = Question)
 
 
 
@@ -64,7 +67,6 @@ get_hw1_questions <- function(login) {
 #' @param path place to write csv could be path, i.e. ~/my_folder/, or null
 #' @return list of dataframes, also write csv files to your disk
 #' @export
-#'
 
 get_hw1_data <- function(login, path = NULL) {
   require(dplyr)
@@ -89,44 +91,44 @@ get_hw1_data <- function(login, path = NULL) {
     print("Beeep")
 
     seed <-  R3PO:::students_2021 %>%
-      filter(name == my_vle_login) %>%
-      select(random_seed) %>%
-      pull()
+      dplyr::filter(name == my_vle_login) %>%
+      dplyr::select(random_seed) %>%
+      dplyr::pull()
 
     set.seed(seed)
 
     airline <-  R3PO:::airline_sub_1 %>%
-      slice_sample(prop = 1)
+      dplyr::slice_sample(prop = 1)
 
     airport <-  R3PO:::airport_sub_1 %>%
-      sample_n(1000) %>%
-      bind_rows(tibble(
-        airport_name = "london-heathrow-airport",
-        link = "/watch?v=ZM04jn95YP0",
-        title = "London Heathrow Airport customer review",
-        author = my_vle_login,
-        author_country = "Russia",
-        date = Sys.Date(),
-        content = "Our first time through Heathrow airport, there was a guide to tell us if they wanted to rebook tickets to Heathrow, so if you are on an urgent flight and you ask, 'Are we going to change your book?' they don't reply. It's a very confusing process, they just tell you: 'Sorry, we did not meet the deadline, but we will do our best to help you.'
-        We can't do anything on the ground at Heathrow, so people have to pick up buses, buy a plane for the day and go in another part of the train. It's a really expensive process, as we have to do it in advance. As soon as we tell you the amount of time it takes, we have to tell them – sometimes we don't have someone on hand for more than 45 minutes. So we'll have to put our hand in hands.
-        At Heathrow, an agent tells passengers, 'The first time we put on a tour agent, I was on a date with a London airport friend, and we went to Heathrow and tried to book a flight directly on to London Heathrow. There were a lot of people who told us that London Heathrow isn't what they expected it to be on the ground at Heathrow, and if we said 'No' to a group of people who asked us to give them their book, we couldn't understand how they were going to respond.
-        ",
-        experience_airport = "transit",
-        date_visit = "2011-11-11",
-        type_traveller = 'Business',
-        overall_rating = 10, queuing_rating = 10,
-        terminal_cleanliness_rating = 10, terminal_seating_rating = 10,
-        terminal_signs_rating = 10, food_beverages_rating = 50,
-        airport_shopping_rating = 10, wifi_connectivity_rating = 10,
-        airport_staff_rating = 10, recommended = 1
-      )) %>%
-      slice_sample(prop = 1)
+      dplyr::sample_n(1000) %>%
+      dplyr::bind_rows(
+        dplyr::tibble(
+          airport_name = "london-heathrow-airport",
+          link = "/watch?v=ZM04jn95YP0",
+          title = "London Heathrow Airport customer review",
+          author = my_vle_login,
+          author_country = "Russia",
+          date = Sys.Date(),
+          content = "Our first time through Heathrow airport, there was a guide to tell us if they wanted to rebook tickets to Heathrow, so if you are on an urgent flight and you ask, 'Are we going to change your book?' they don't reply. It's a very confusing process, they just tell you: 'Sorry, we did not meet the deadline, but we will do our best to help you.'
+          We can't do anything on the ground at Heathrow, so people have to pick up buses, buy a plane for the day and go in another part of the train. It's a really expensive process, as we have to do it in advance. As soon as we tell you the amount of time it takes, we have to tell them – sometimes we don't have someone on hand for more than 45 minutes. So we'll have to put our hand in hands.
+          At Heathrow, an agent tells passengers, 'The first time we put on a tour agent, I was on a date with a London airport friend, and we went to Heathrow and tried to book a flight directly on to London Heathrow. There were a lot of people who told us that London Heathrow isn't what they expected it to be on the ground at Heathrow, and if we said 'No' to a group of people who asked us to give them their book, we couldn't understand how they were going to respond.",
+          experience_airport = "transit",
+          date_visit = "2011-11-11", type_traveller = 'Business',
+          overall_rating = 10, queuing_rating = 10,
+          terminal_cleanliness_rating = 10, terminal_seating_rating = 10,
+          terminal_signs_rating = 10, food_beverages_rating = 50,
+          airport_shopping_rating = 10, wifi_connectivity_rating = 10,
+          airport_staff_rating = 10, recommended = 1
+        )
+      ) %>%
+      dplyr::slice_sample(prop = 1)
 
     lounge <- R3PO:::lounge_sub_1 %>%
-      slice_sample(prop = 1)
+      dplyr::slice_sample(prop = 1)
 
     seat <- R3PO:::seat_sub_1 %>%
-      slice_sample(prop = 1)
+      dplyr::slice_sample(prop = 1)
 
     myList <- list(airline = airline,
                    airport = airport,
