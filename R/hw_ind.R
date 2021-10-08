7be5fe9311c66a1f0ac7e67edb9c3bf2eb1bb4d8
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-08 15:24:51 +0300

    Add files via upload

--- R/hw_ind.R
+++ R/hw_ind.R
@@ -18,9 +18,31 @@ get_hw_ind_questions <- function(login) {
     my_vle_login = login
   }
 
-  if (!my_vle_login %in% R3PO:::ind_task_final$login) {
+  if (!my_vle_login %in% R3PO:::students_2021$name) {
     print("invalid login")
   } else {
+
+    seed = R3PO:::students_2021 %>%
+      filter(name == my_vle_login) %>%
+      select(random_seed) %>%
+      pull()
+
+    set.seed(seed)
+
+    df_q = read.csv("data/hw1_questions.csv")
+
+    q1 = df_q %>%
+      filter(Comment == "1 таблица") %>%
+      sample_n(1) %>% pull(var = Question)
+
+    q2 = df_q %>%
+      filter(Comment == "с текстами") %>%
+      sample_n(1) %>% pull(var = Question)
+
+    q3 = df_q %>%
+      filter(Comment == "2 таблицы") %>%
+      sample_n(1) %>% pull(var = Question)
+
     # test_team = ind_login %>%
     #   dplyr::filter(login == my_vle_login) %>%
     #   dplyr::pull(team)
